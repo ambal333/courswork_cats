a7a5a32d1838df4e2d174fec22d756bb52546422
ambal777 <vpasko474@gmail.com>
2026-06-16 23:16:14 +0300

    разделил на 4 класса

--- main.py
+++ main.py
@@ -3,12 +3,9 @@ import json
 import os
 # from API_TOKEN import API_KEY
 class GetCats:
-    json_data=[]
     _url_cats='https://cataas.com/'
-    _yandex_url='https://cloud-api.yandex.net/v1/disk/'
-    def __init__(self,text,api_key):
+    def __init__(self,text):
         self.text=text
-        self.api_key=api_key
     def _build_url(self,api_method):
         return f'{self._url_cats}{api_method}'
     def getcat(self):
@@ -18,6 +15,10 @@ class GetCats:
         data = response.json()
         print('фото кота получено')
         return data.get('url')
+class YandexDisc:
+    _yandex_url='https://cloud-api.yandex.net/v1/disk/'
+    def __init__(self,api_key):
+        self.api_key=api_key
     def _new_folder(self,group):
         params={'path':group}
         headers = {'Authorization': f'OAuth {self.api_key}'}
@@ -26,27 +27,27 @@ class GetCats:
         print('папка создана')
         return data.get('href')
 
-    def import_disc(self):
-        cat_url=self.getcat()
+    def import_disc(self,photo_cat_url,text_photo):
+        cat_url=photo_cat_url
         group='PY-254'
         self._new_folder(group)
-        params={'url':cat_url,'path':f'{group}/cat_{self.text}.jpg' }
+        params={'url':cat_url,'path':f'{group}/cat_{text_photo}.jpg' }
         headers={'Authorization': f'OAuth {self.api_key}'}
         response=requests.post(f'{self._yandex_url}resources/upload',params=params,headers=headers)
         response.raise_for_status()
         data=response.json()
-        info_photo={'name': f'cat_{self.text}', 'text': self.text,'link':cat_url}
-        self._load_existing_report()
-        self._json_file_append_info(info_photo)
-        return 'фото добавлены'
-    def _load_existing_report(self):
+        info_photo={'name': f'cat_{text_photo}', 'text': text_photo,'link':cat_url}
+        print('фото добавлены')
+        return info_photo
+class CreateJsonfile:
+    json_data = []
+    def load_existing_report(self):
         if os.path.exists('info_cats/backup_report.json'):
             with open('info_cats/backup_report.json',encoding='utf-8') as f:
                 self.json_data=json.load(f)
         else:
             self.json_data = []
-
-    def _json_file_append_info(self,new_info):
+    def json_file_append_info(self,new_info):
         self.json_data.append(new_info)
         os.makedirs('info_cats', exist_ok=True)
         with open('info_cats/backup_report.json','w',encoding='utf-8') as f:
@@ -54,7 +55,13 @@ class GetCats:
             f.flush()
             json_str=json.dumps(self.json_data,ensure_ascii=False,indent=2)
             print(json_str)
-
-cat=GetCats('hello',API_KEY)
-res=cat.import_disc()
-print(res)
+def main():
+    cat_photo=GetCats('hello')
+    url = cat_photo.getcat()
+    yandex=YandexDisc(API_KEY)
+    info_photo = yandex.import_disc(url,cat_photo.text)
+    json_file = CreateJsonfile()
+    json_file.load_existing_report()
+    json_file.json_file_append_info(info_photo)
+if __name__=='__main__':
+    main()
